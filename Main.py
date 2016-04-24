376ec56bfb45a6d1d6ad84cc4b671914601b5889
Ishancorp <sharma.ishan450@gmail.com>
2016-04-24 16:42:21 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,11 +17,12 @@ screen = pygame.display.set_mode((1015, 770))
 
 #Loading images and initializing list to store them in
 back = pygame.image.load("Game initial sketch.png").convert() #the background pic needs to be 1015x595 px. 
-ball = pygame.image.load("small_ball.png").convert()
+ball = pygame.image.load("small_ball.png").convert() #must be 35x35, or (some multiple of 35)x(some multiple of 35)
 img = []
 
 #Setting up some colours
 dark_gray=(75,75,75)
+pressed=(125,125,125)
 gray=(200,200,200)
 light_gray=(242, 242, 242)
 black=(0,0,0)
@@ -31,10 +32,8 @@ black=(0,0,0)
 bottom_bounds=155
 background = pygame.Surface(screen.get_size()).convert()
 background.fill(light_gray)
-border=pygame.Surface((102,bottom_bounds+2)).convert()
+border=pygame.Surface((122,bottom_bounds+2)).convert()
 border.fill(dark_gray)
-ship = pygame.Surface((100,bottom_bounds)).convert()
-ship.fill(gray)
 main_border=pygame.Surface((527,bottom_bounds+2)).convert()
 main_border.fill(dark_gray)
 display = pygame.Surface((525,bottom_bounds)).convert()
@@ -44,14 +43,22 @@ display.fill(gray)
 font = pygame.font.SysFont("helvetica", 14)
 tank_text = font.render("Tank", True, black)
 tank_cost = font.render("$1 000", True, black)
-missile_text = font.render("Missile", True, black)
+missile_text = font.render("Missile Launcher", True, black)
 missile_cost = font.render("$10 000", True, black)
 mine_text = font.render("Sea Mine", True, black)
 mine_cost = font.render("$50 000", True, black)
 
+tank_pressed=True
+missile_pressed=False
+seamine_pressed=False
 #setting up coordinate variables
 x=0
 y=0
+money=0
+chances=3
+pause=False
+ships_destroyed=0
+ships_remaining=0
 
 #Game engine
 clock = pygame.time.Clock()
@@ -61,27 +68,64 @@ while keep_going:
     for ev in pygame.event.get():
         if ev.type == QUIT:
             keep_going=False
-        elif ev.type==MOUSEBUTTONDOWN:
+        elif ev.type==MOUSEBUTTONDOWN: #if the mouse is pressed
             x=((ev.pos[0])//35)*35 #getting the x of where the mouse clicked
             y=((ev.pos[1])//35)*35 #getting the y of where the mouse clicked
-            if 111>x>9 and 136>y>9: #if the tank part of the menu is pressed
+            if 131>x>9 and 136>y>9: #if the tank part of the menu is pressed
                 print("Tank")
-            elif 230>x>119 and 136>y>9: #if the missile part of the menu is pressed
+                tank_pressed=True
+                missile_pressed=False
+                seamine_pressed=False
+            elif 261>x>139 and 136>y>9: #if the missile part of the menu is pressed
                 print("Missile")
-            elif 340>x>229 and 136>y>9: #if the sea mine part of the menu is pressed
+                tank_pressed=False
+                missile_pressed=True
+                seamine_pressed=False
+            elif 391>x>269 and 136>y>9: #if the sea mine part of the menu is pressed
                 print("Sea Mine")
+                tank_pressed=False
+                missile_pressed=False
+                seamine_pressed=True
             elif y<175: #if any other part of the menu is pressed
-                print("Pause")
+                if pause==True: #if the game is already paused
+                    pause=False
+                    print("Unpaused")
+                elif pause==False: #if the game is not paused
+                    pause=True
+                    print("Paused")
             else: #if the main gameplay part is pressed
                 overlap=False
                 for image in img:
-                    if image[1]==x and image[2]==y:
+                    if image[1]==x and image[2]==y: #if the 
                         overlap=True
                         print("Overlap")
                 #overlap is true if the x and y of the sprite is already covered
                 if overlap==False:
                     img.append([ball,x,y])
     #print(x,y)
+    
+    tank_surface = pygame.Surface((120,bottom_bounds)).convert()
+    missile_surface = pygame.Surface((120,bottom_bounds)).convert()
+    seamine_surface = pygame.Surface((120,bottom_bounds)).convert()
+    if tank_pressed==False:
+        tank_surface.fill(gray)
+    else:
+        tank_surface.fill(pressed)
+    if missile_pressed==False:
+        missile_surface.fill(gray)
+    else:
+        missile_surface.fill(pressed)
+    if seamine_pressed==False:
+        seamine_surface.fill(gray)
+    else:
+        seamine_surface.fill(pressed)
+    
+    money_text = font.render("Money: $"+str(money), True, black)
+    numweapons_text = font.render("Weapons: "+str(len(img)), True, black)
+    islandsdestroyed_text = font.render("Islands Destroyed: "+str(3-chances), True, black)
+    chance_text = font.render("Chances: "+str(chances), True, black)
+    shipsdestroyed_text = font.render("Ships Destroyed: "+str(ships_destroyed), True, black)
+    shipsremaining_text = font.render("Ships Remaining: "+str(ships_remaining), True, black)
     
     #Blitting
     screen.blit(background, (0,0))
@@ -91,21 +135,30 @@ while keep_going:
     #print(img)
     top_bounds=10
     screen.blit(border, (9,top_bounds-1))
-    screen.blit(ship, (10,top_bounds))
-    screen.blit(border, (119,top_bounds-1))
-    screen.blit(ship, (120,top_bounds))
-    screen.blit(border, (229,top_bounds-1))
-    screen.blit(ship, (230,top_bounds))
-    screen.blit(main_border, (479,top_bounds-1))
-    screen.blit(display, (480,top_bounds))
-    
-    screen.blit(tank_text, (27, 100))
-    screen.blit(missile_text, (137, 100))
-    screen.blit(mine_text, (247, 100))
-    
-    screen.blit(tank_cost, (27, 115))
-    screen.blit(missile_cost, (137, 115))
-    screen.blit(mine_cost, (247, 115))
+    screen.blit(tank_surface, (10,top_bounds))
+    screen.blit(border, (139,top_bounds-1))
+    screen.blit(missile_surface, (140,top_bounds))
+    screen.blit(border, (269,top_bounds-1))
+    screen.blit(seamine_surface, (270,top_bounds))
+    screen.blit(main_border, (469,top_bounds-1))
+    screen.blit(display, (470,top_bounds))
+
+    scale=40
+    top_bounds=100
+    screen.blit(tank_text, (scale, top_bounds))
+    screen.blit(missile_text, (110+scale, top_bounds))
+    screen.blit(mine_text, (250+scale, top_bounds))
+    screen.blit(tank_cost, (scale, top_bounds+15))
+    screen.blit(missile_cost, (scale+110, top_bounds+15))
+    screen.blit(mine_cost, (250+scale, top_bounds+15))
+    screen.blit(islandsdestroyed_text, (500, top_bounds+15))
+    screen.blit(chance_text, (750, top_bounds+15))
+    screen.blit(shipsdestroyed_text, (500, top_bounds-25))
+    screen.blit(shipsremaining_text, (750, top_bounds-25))
+    screen.blit(money_text, (500, top_bounds-65))
+    screen.blit(numweapons_text, (750, top_bounds-65))
+
+    screen.blit(ball, (scale+3,50))
     pygame.display.flip()
 
 pygame.display.quit()
